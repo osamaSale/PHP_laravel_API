65e3c84fc3e769170897de3395198f9378c4f91a
osamaSale <osama.mohamad.salem@gmail.com>
2023-04-07 07:37:55 +0300

    title of the changes you did

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -16,14 +17,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+ // Public Routes Products
+
+
+/* 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-Route::get('/products/search/{name}', [ProductController::class, 'search']);
+*/
+
+// OR
+
+Route::resource('/products', ProductController::class); 
+Route::get('/products/search/{name}', [ProductController::class, 'search']); 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Public Router Users
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users', [AuthController::class, 'users']);
+//Route::get('/users', [AuthController::class, 'logout']);
+
+
+// Protected router
+
+Route::group(['middleware' => ['auth:sanctum']],function(){
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
